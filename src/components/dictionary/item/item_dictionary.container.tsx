c7d8591d045ccbad0517ac5d9fd8475d40b74bec
Vladimir <BoW321@ya.ru>
2023-09-14 11:11:04 +0300

    style

--- src/components/dictionary/item/item_dictionary.container.tsx
+++ src/components/dictionary/item/item_dictionary.container.tsx
@@ -1,48 +1,12 @@
 import { FC, memo } from 'react';
 import { DictionaryModel } from '../../../models/state/dictionary_state.model';
-import { Button, ButtonProps, Checkbox, Stack, styled } from '@mui/material';
+import { Button, Checkbox, Stack, styled } from '@mui/material';
 import { Close } from '@mui/icons-material';
 
 interface IItemDictionary extends DictionaryModel {
     handleOnChecked: (id: string) => void;
     handleDelete: (id: string) => void;
 }
-
-const BootstrapButton = styled(Button)({
-    boxShadow: 'none',
-    textTransform: 'none',
-    fontSize: 16,
-    padding: '6px 12px',
-    border: '1px solid',
-    lineHeight: 1.5,
-    backgroundColor: '#0063cc',
-    borderColor: '#0063cc',
-    fontFamily: [
-        '-apple-system',
-        'BlinkMacSystemFont',
-        '"Segoe UI"',
-        'Roboto',
-        '"Helvetica Neue"',
-        'Arial',
-        'sans-serif',
-        '"Apple Color Emoji"',
-        '"Segoe UI Emoji"',
-        '"Segoe UI Symbol"',
-    ].join(','),
-    '&:hover': {
-        backgroundColor: '#0069d9',
-        borderColor: '#0062cc',
-        boxShadow: 'none',
-    },
-    '&:active': {
-        boxShadow: 'none',
-        backgroundColor: '#0062cc',
-        borderColor: '#005cbf',
-    },
-    '&:focus': {
-        boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
-    },
-});
 
 const ColorButton = styled(Button)({
     height: '30px',
